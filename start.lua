bb6bfdfd1081b7238e8dd752c79d032a7ac99c9b
TeAmSnaBv <89605987+TeAmSnaBv@users.noreply.github.com>
2021-08-26 22:45:23 +0300

    Add files via upload

--- start.lua
+++ start.lua
@@ -1,4 +1,4 @@
-Create_1 = dofile("./File_Libs/TeAmSnaB.lua")
+Create_1 = dofile("./File_Libs/serpent.lua")
 Create_2 = require("ssl.https")
 Create_4 = io.popen("echo $SSH_CLIENT | awk '{ print $1}'"):read('*a')
 local Create_3 = dofile("./File_Libs/redis.lua").connect("127.0.0.1", 6379)
@@ -12,7 +12,7 @@ Sudo = ]]..Sudo..[[
 ]])
 Create_7:close()
 end  
-if not Create_3:get(Create_4.."Token_spider") then
+if not Create_3:get(Create_4.."Token_TeAmSnaB") then
 print("\27[1;34m»» Send Your Token Bot :\27[m")
 local token = io.read()
 if token ~= '' then
@@ -21,30 +21,30 @@ if res ~= 200 then
 io.write('\n\27[1;31m»» Sorry The Token is not Correct \n\27[0;39;49m')
 else
 io.write('\n\27[1;31m»» The Token Is Saved\n\27[0;39;49m')
-Create_3:set(Create_4.."Token_spider",token)
+Create_3:set(Create_4.."Token_TeAmSnaB",token)
 end 
 else
 io.write('\n\27[1;31mThe Tokem was not Saved\n\27[0;39;49m')
 end 
 os.execute('lua start.lua')
 end
-if not Create_3:get(Create_4.."Id_spider") then
+if not Create_3:get(Create_4.."Id_TeAmSnaB") then
 print("\27[1;34m\n»» Send Your id Sudo : \27[m")
 local Id = io.read():gsub(' ','') 
 if tostring(Id):match('%d+') then
 io.write('\n\27[1;31m»» The id Is Saved\n\27[0;39;49m')
-Create_3:set(Create_4.."Id_spider",Id)
+Create_3:set(Create_4.."Id_TeAmSnaB",Id)
 io.write('\n\27[1;31m»» Sorry The id is not Correct \n\27[0;39;49m')
 os.execute('lua start.lua')
 end
 end
-local function Files_spider_Info()
-Create_6(Create_3:get(Create_4.."Token_spider"),Create_3:get(Create_4.."Id_spider"))   
-local Runspider = io.open("TeAmSnaB", 'w')
-Runspider:write([[
+local function Files_TeAmSnaB_Info()
+Create_6(Create_3:get(Create_4.."Token_TeAmSnaB"),Create_3:get(Create_4.."Id_TeAmSnaB"))   
+local RunTeAmSnaB = io.open("TeAmSnaB", 'w')
+RunTeAmSnaB:write([[
 #!/usr/bin/env bash
 cd $HOME/TeAmSnaB
-token="]]..Create_3:get(Create_4.."Token_spider")..[["
+token="]]..Create_3:get(Create_4.."Token_TeAmSnaB")..[["
 rm -fr TeAmSnaB.lua
 wget "https://raw.githubusercontent.com/TeAmSnaBv/TeAmSnaB/master/TeAmSnaB.lua"
 while(true) do
@@ -52,7 +52,7 @@ rm -fr ../.telegram-cli
 ./tg -s ./TeAmSnaB.lua -p PROFILE --bot=$token
 done
 ]])
-Runspider:close()
+RunTeAmSnaB:close()
 local RunTs = io.open("Run", 'w')
 RunTs:write([[
 #!/usr/bin/env bash
@@ -65,9 +65,9 @@ done
 ]])
 RunTs:close()
 end
-Files_spider_Info()
-Create_3:del(Create_4.."Token_spider")
-Create_3:del(Create_4.."Id_spider")
+Files_TeAmSnaB_Info()
+Create_3:del(Create_4.."Token_TeAmSnaB")
+Create_3:del(Create_4.."Id_TeAmSnaB")
 sudos = dofile('sudo.lua')
 os.execute('./install.sh ins')
 end 
@@ -78,8 +78,8 @@ Create_5()
 var = true
 else   
 f:close()  
-Create_3:del(Create_4.."Token_spider")
-Create_3:del(Create_4.."Id_spider")
+Create_3:del(Create_4.."Token_TeAmSnaB")
+Create_3:del(Create_4.."Id_TeAmSnaB")
 sudos = dofile('sudo.lua')
 os.execute('./install.sh ins')
 var = false
